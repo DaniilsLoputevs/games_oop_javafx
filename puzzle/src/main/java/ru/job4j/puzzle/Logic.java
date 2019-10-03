dcc2f8cb25ea4913cbac338ec565220bb72626b1
DaniilsLoputevs <laiwiense@gmail.com>
2019-10-03 14:54:03 +0100

    add isWin();

--- puzzle/src/main/java/ru/job4j/puzzle/Logic.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Logic.java
@@ -71,6 +71,22 @@ public class Logic {
     public boolean isWin() {
         int[][] table = this.convert();
         boolean result = false;
+        for (int row = 0; row < table.length; row++) {
+            int winRowCount = 0;
+            int winCellCount = 0;
+            for (int cell = 0; cell < table.length; cell++) {
+                if (table[row][cell] == 1) {
+                    winRowCount++;
+                }
+                if (table[cell][row] == 1) {
+                    winCellCount++;
+                }
+                if (winCellCount == table.length || winRowCount == table.length) {
+                    result = true;
+                    break;
+                }
+            }
+        }
         return result;
     }
 
