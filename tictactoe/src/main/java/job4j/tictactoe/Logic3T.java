ceb39c91ba8dac1c1b434ce52de83d2f6cc2902b
DaniilsLoputevs <laiwiense@gmail.com>
2020-01-12 21:21:50 +0000

    Крестики-нолики на JavaFX[#178993]

--- tictactoe/src/main/java/job4j/tictactoe/Logic3T.java
+++ tictactoe/src/main/java/job4j/tictactoe/Logic3T.java
@@ -1,6 +1,11 @@
 package job4j.tictactoe;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
  * Логика Игрового поля.
@@ -72,6 +77,67 @@ public class Logic3T {
      * @return boolean (true/false)
      */
     public boolean hasGap() {
-        return true;
+//        return Arrays.stream(table)
+//                .flatMap(figure3TS -> Arrays.stream(figure3TS)
+//                        .filter(figure3T -> !figure3T.hasMarkX() && !figure3T.hasMarkO()).collect(Collector.of(
+//                                (figure3TS -> )
+//                        )))
+//                .findFirst().isPresent();
+
+//                ))
+//                .findFirst()
+
+//        // Рабочий вариант.
+//        boolean isFree = false;
+//        for (Figure3T[] arr : table) {
+//            for (Figure3T cell : arr) {
+//                if (!cell.hasMarkX() && !cell.hasMarkO()) {
+//                    isFree = true;
+//                    break;
+//                }
+//            }
+//            if (isFree) { break; }
+//        }
+//        return isFree;
+
+        return Arrays.stream(table)
+                .flatMap(Arrays::stream)
+                .flatMap(figure3T -> {
+                    boolean check = false;
+                    if (!figure3T.hasMarkO() && !figure3T.hasMarkX()) {
+                    return check;
+                } return  check; } );
+
+
+//                .forEach(figure3T -> {if (!figure3T.hasMarkO() && !figure3T.hasMarkX()) {
+//                return true;
+//                }
+//                } );
+
+
+
+//        return
+//        List<Figure3T> check = Arrays.stream(table)
+//                .flatMap(Arrays::stream)
+//                .filter(figure3T -> !figure3T.hasMarkX() && !figure3T.hasMarkO())
+//                .collect(
+
+//                         (set, holder) -> set.add(holder.value),
+//                         (left, right) -> { left.addAll(right); return left; }
+//                ));
+
+//        return (check != null) ? true : false;
+
+//                .flatMap(figure3TStream -> figure3TStream.filter())
+//                .filter()
+//                .flatMap(figure3T -> figure3T.)
+//
+//                .map()
+
+
+//                int[][] arr = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+//IntStream stream = Arrays.stream(arr).flatMapToInt(x -> Arrays.stream(x));
+
+//        return true;
     }
 }
